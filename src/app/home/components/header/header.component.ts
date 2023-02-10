c6b5302634d073a73eec4ee60333653cfda96391
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-10 14:16:38 -0300

    popover event

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -1,5 +1,6 @@
+import { PopoverComponent } from './popover/popover.component';
 import { Component, OnInit } from '@angular/core';
-
+import { PopoverController } from '@ionic/angular';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  roleMsg:string = '';
+
+  constructor(public popoverController: PopoverController) { }
 
   ngOnInit() {}
+
+  async presentPopover(e: Event) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      event: e,
+    });
+
+    await popover.present();
+
+    const { role } = await popover.onDidDismiss();
+    this.roleMsg = `Popover dismissed with role: ${role}`;
+  }
 
 }
