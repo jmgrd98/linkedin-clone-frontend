de11266baba898c48438111233074849b22c8709
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-22 10:10:57 -0300

    commit

---
+++ src/app/home/models/User.ts
@@ -0,0 +1,8 @@
+export type Role = 'admin' | 'premium' | 'user';
+export interface User{
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: Role;
+}
--- src/app/home/services/post.service.ts
+++ src/app/home/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Post } from './../models/Post';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,10 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-  getSelectedPost(params:any){ 
+  getSelectedPost(params:any){
 
-    return this.http.get<Post[]>('http://localhost:3000/api/feed' + params);
-
+    return this.http.get<Post[]>(
+      `${environment.baseApiUrl}/feed${params}`);
   }
 
 
