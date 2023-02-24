ca7706cd78233696f530c67eee7ea225322d82d0
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-24 16:09:34 -0300

    commit

--- src/app/home/services/post.service.ts
+++ src/app/home/services/post.service.ts
@@ -1,7 +1,8 @@
 import { Post } from './../models/Post';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from "../../../environments/environment";
+import {take} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,28 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
+  private httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   getSelectedPost(params:any){
 
     return this.http.get<Post[]>(
       `${environment.baseApiUrl}/feed${params}`);
   }
 
+  createPost(body: string) {
+    return this.http.post<Post>(`${environment.baseApiUrl}/feed`, { body }, this.httpOptions)
+      .pipe(
+      take(1)
+    );
+  }
+
+  updatePost(body: string, postId: number) {
+    return this.http.put<Post>(`${environment.baseApiUrl}/feed${postId}`, { body }, this.httpOptions)
+      .pipe(
+        take(1)
+      );
+  }
 
 }
